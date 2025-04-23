958072535e8d81de63f8d6a95da425dea401f77b
Agvan <101641443+AgvanGrigoryan@users.noreply.github.com>
2025-04-23 23:16:55 +0400

    Registration request and response schemas and token schemas

---
+++ app/schemas/token.py
@@ -0,0 +1,9 @@
+
+
+class BaseTokensResponse(BaseModel):
+	access_token: str
+	refresh_token: str
+	token_type: str
+
+class JWTTokensResponse(BaseTokensResponse):
+	token_type: str = "bearer"
---
+++ app/schemas/user.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, EmailStr, SecretStr, field_validator
+from typing import Optional
+import string
+from app.schemas.token import JWTTokensResponse
+
+class UserRegisterRequest(BaseModel):
+	name: str
+	surname: str
+	email: EmailStr
+	password: SecretStr
+	description: Optional[str] = None
+
+	@field_validator("password", mode="after")
+	def validate_password(cls, password: SecretStr) -> SecretStr:
+		actual_pass = password.get_secret_value()
+		if len(actual_pass) < 8:
+			raise ValueError("Password must be longer than 8 symbols")
+		has_digit = any(char.isdigit() for char in actual_pass)
+		has_upper = any(char.isupper() for char in actual_pass)
+		has_symbol = any(char in string.punctuation for char in actual_pass)
+		if not (has_digit and has_upper and has_symbol):
+			raise ValueError("Password must include at least one digit, one uppercase letter and one special symbol")
+		return password
+	
+class UserRegisterRequest(JWTTokensResponse):
+	pass
